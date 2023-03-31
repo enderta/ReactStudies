f59b79e9e7d3345a00e167898eed809f9e1fe3bd
enderta <endertanver@gmail.com>
2023-03-31 17:50:16 +0100

    book

--- src/components/guttenberg/BookCards.js
+++ src/components/guttenberg/BookCards.js
@@ -1,31 +1,36 @@
 import React from 'react';
-import {Card} from "react-bootstrap";
+import {Card, Row} from "react-bootstrap";
 
 
 const BookCards = (props) => {
 
     return (
+        <div>
+            <div className={"container"} >
+                <div className={"row"}>
+                    <div className={"col-6"}>
 
+                                <Card  key={props.book.key} className="cards" style={{ width: '18rem' }}>
+                                    <Card.Img
 
+                                        src={`http://covers.openlibrary.org/b/id/${props.book.cover_i}-M.jpg`}
+                                        style={{ height: '300px', objectFit: 'cover' }}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>{props.book.title}</Card.Title>
+                                    </Card.Body>
 
-                <Card  key={props.book.key} className="cards" style={{ width: '18rem' }}>
-                    <Card.Img
+                                    <Card.Text>{props.book.author_name}</Card.Text>
+                                    <a href={`https://openlibrary.org${props.book.key}`} target="_blank">
+                                        Download
+                                    </a>
 
-                        src={`http://covers.openlibrary.org/b/id/${props.book.cover_i}-M.jpg`}
-                        style={{ height: '300px', objectFit: 'cover' }}
-                    />
-                    <Card.Body>
-                        <Card.Title>{props.book.title}</Card.Title>
-                    </Card.Body>
+                                </Card>
 
-                        <Card.Text>{props.book.author_name}</Card.Text>
-                        <a href={`https://openlibrary.org${props.book.key}`} target="_blank">
-                            Download
-                        </a>
-
-                </Card>
-
-
+                            </div>
+                        </div>
+                    </div>
+        </div>
 
     );
 };
