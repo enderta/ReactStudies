f8d19c8ffb5903d7cfc4e47825c67df53dee1aae
enderta <endertanver@gmail.com>
2023-04-09 18:53:12 +0100

    books

--- server/commerce.js
+++ server/commerce.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { Pool } = require("pg");
+const {Pool} = require("pg");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { check, validationResult } = require("express-validator");
+const {check, validationResult} = require("express-validator");
 const secret = "secret";
 
 app.use(cors());
@@ -26,24 +26,24 @@ app.post(
         check(
             "password",
             "Please enter a password with 6 or more characters"
-        ).isLength({ min: 6 }),
-        check("name", "Please enter a name").isLength({ min: 1 }),
-        check("address", "Please enter an address").isLength({ min: 1 }),
-        check("phone", "Please enter a phone number").isLength({ min: 1 }),
+        ).isLength({min: 6}),
+        check("name", "Please enter a name").isLength({min: 1}),
+        check("address", "Please enter an address").isLength({min: 1}),
+        check("phone", "Please enter a phone number").isLength({min: 1}),
         check("is_admin", "Please enter a boolean value").isBoolean(),
     ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({errors: errors.array()});
         }
 
-        const { email, password, name, address, phone, is_admin } = req.body;
+        const {email, password, name, address, phone, is_admin} = req.body;
 
         try {
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(password, salt);
-            const { rows } = await pool.query(
+            const {rows} = await pool.query(
                 "INSERT INTO users (email, password, name, address, phone, is_admin) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
                 [email, hashedPassword, name, address, phone, is_admin]
             );
@@ -59,7 +59,7 @@ app.post(
                     },
                 },
                 secret,
-                { expiresIn: "1h" }
+                {expiresIn: "1h"}
             );
             res.status(201).json({
                 status: "success",
@@ -91,25 +91,25 @@ app.post(
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({errors: errors.array()});
         }
 
-        const { email, password } = req.body;
+        const {email, password} = req.body;
 
         try {
-            const { rows } = await pool.query(
+            const {rows} = await pool.query(
                 "SELECT * FROM users WHERE email = $1",
                 [email]
             );
             if (rows.length === 0) {
                 return res.status(400).json({
-                    errors: [{ msg: "Invalid credentials" }],
+                    errors: [{msg: "Invalid credentials"}],
                 });
             }
             const isMatch = await bcrypt.compare(password, rows[0].password);
             if (!isMatch) {
                 return res.status(400).json({
-                    errors: [{ msg: "Invalid credentials" }],
+                    errors: [{msg: "Invalid credentials"}],
                 });
             }
             const token = jwt.sign(
@@ -124,7 +124,7 @@ app.post(
                     },
                 },
                 secret,
-                { expiresIn: "1h" }
+                {expiresIn: "1h"}
             );
             res.status(200).json({
                 status: "success",
@@ -143,45 +143,193 @@ app.post(
 
 //only admin can get all users
 //10 users per page
-let page=1;
+let page = 1;
 app.get("/users", async (req, res) => {
     jwt.verify(req.headers.authorization, secret, async (error, decoded) => {
+            if (error) {
+                res.status(401).json({error: "Unauthorized"});
+            } else {
+                if (!decoded.user.is_admin) {
+                    res.status(401).json({error: "Unauthorized"});
+                    return;
+                }
+
+                const searchTerm = req.query.search;
+                let query = "SELECT * FROM users";
+
+                if (searchTerm) {
+                    const escapedSearchTerm = `%${searchTerm}%`;
+                    query += " WHERE name ILIKE $1 OR email ILIKE $1";
+                    const {rows} = await pool.query(query, [escapedSearchTerm]);
+                   if(rows.length === 0) {
+                       res.status(404).json({
+                           status: "fail",
+                           message: "No users found"
+                       })
+                   } else {
+                       res.status(200).json({
+                           status: "success",
+                           message: `Users matching search term: ${searchTerm}`,
+                           data: rows,
+                       });
+                   }
+                } else {
+                    query += " LIMIT 10 OFFSET $1";
+                    const {rows} = await pool.query(query, [(page - 1) * 10]);
+                    res.status(200).json({
+                        status: "success",
+                        message: "All users",
+                        data: rows,
+                        page: page,
+                        total: rows.length,
+                    });
+                }
+            }
+        }
+    )
+});
+
+//only admin can get a user
+app.get("/users/:id", async (req, res) => {
+    jwt.verify(req.headers.authorization, secret, async (error, decoded) => {
         if (error) {
-            res.status(401).json({ error: "Unauthorized" });
+            res.status(401).json({error: "Unauthorized"});
         } else {
             if (!decoded.user.is_admin) {
-                res.status(401).json({ error: "Unauthorized" });
+                res.status(401).json({error: "Unauthorized"});
                 return;
             }
-
-            const searchTerm = req.query.search;
-            let query = "SELECT * FROM users";
-
-            if (searchTerm) {
-                const escapedSearchTerm = `%${searchTerm}%`;
-                query += " WHERE name ILIKE $1 OR email ILIKE $1";
-                const { rows } = await pool.query(query, [escapedSearchTerm]);
-                res.status(200).json({
-                    status: "success",
-                    message: `Users matching search term "${searchTerm}"`,
-                    data: rows,
+            const {rows} = await pool.query(
+                "SELECT * FROM users WHERE id = $1",
+                [req.params.id]
+            );
+            if (rows.length === 0) {
+                res.status(404).json({
+                    status: "error",
+                    message: "User not found",
                 });
             } else {
-                query += " LIMIT 10 OFFSET $1";
-                const { rows } = await pool.query(query, [(page-1)*10]);
                 res.status(200).json({
                     status: "success",
-                    message: "All users",
-                    data: rows,
+                    message: "User found",
+                    data: rows[0],
                 });
             }
         }
-    }
-)});
+    });
+});
 
+//only admin can update a user
+app.put("/users/:id", async (req, res) => {
+    jwt.verify(req.headers.authorization, secret, async (error, decoded) => {
+            if (error) {
+                res.status(401).json({error: "Unauthorized"});
+            } else {
+                if (!decoded.user.is_admin) {
+                    res.status(401).json({error: "Unauthorized"});
+                    return;
+                }
+                const {email, password, name, address, phone, is_admin} = req.body;
+                const {rows} = await pool.query(
+                    "SELECT * FROM users WHERE id = $1",
+                    [req.params.id]
+                );
+                if (rows.length === 0) {
+                    res.status(404).json({
+                        status: "error",
+                        message: "User not found",
+                    });
+                }
+                if (email) {
+                    const {rows} = await pool.query(
+                        "SELECT * FROM users WHERE email = $1",
+                        [email]
+                    );
+                    if (rows.length > 0) {
+                        res.status(400).json({
+                            status: "error",
+                            message: "Email already exists",
+                        });
+                        return;
+                    }
+                }
+                if (password) {
+                    const salt = await bcrypt.genSalt(10);
+                    const hashedPassword = await bcrypt.hash(password, salt);
+                    await pool.query(
+                        "UPDATE users SET password = $1 WHERE id = $2",
+                        [hashedPassword, req.params.id]
+                    );
+                }
+                if (name) {
+                    await pool.query(
+                        "UPDATE users SET name = $1 WHERE id = $2",
+                        [name, req.params.id]
+                    );
+                }
+                if (address) {
+                    await pool.query(
+                        "UPDATE users SET address = $1 WHERE id = $2",
+                        [address, req.params.id]
+                    );
+                }
+                if (phone) {
+                    await pool.query(
+                        "UPDATE users SET phone = $1 WHERE id = $2",
+                        [phone, req.params.id]
+                    );
+                }
+                if (is_admin) {
+                    await pool.query(
+                        "UPDATE users SET is_admin = $1 WHERE id = $2",
+                        [is_admin, req.params.id]
+                    );
+                }
+                res.status(200).json({
+                    status: "success",
+                    message: "User updated",
+                });
+
+            }
+        }
+    );
+});
+
+//only admin can delete a user
+app.delete("/users/:id", async (req, res) => {
+        jwt.verify(req.headers.authorization, secret, async (error, decoded) => {
+            if (error) {
+                res.status(401).json({error: "Unauthorized"});
+            } else {
+                if (!decoded.user.is_admin) {
+                    res.status(401).json({error: "Unauthorized"});
+                    return;
+                }
+                const {rows} = await pool.query(
+                    "SELECT * FROM users WHERE id = $1",
+                    [req.params.id]
+                );
+                if (rows.length === 0) {
+                    res.status(404).json({
+                        status: "error",
+                        message: "User not found",
+                    });
+                } else {
+                    await pool.query("DELETE FROM users WHERE id = $1", [
+                        req.params.id,
+                    ]);
+                    res.status(200).json({
+                        status: "success",
+                        message: "User deleted",
+                    });
+                }
+            }
+        });
+    }
+);
 
 
 app.listen(3001, () => {
-    console.log("Server is listening on port 3001");
-}
+        console.log("Server is listening on port 3001");
+    }
 );
