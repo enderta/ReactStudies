66e57acd819b6040a18644bcf10a4a4e89d8a9d7
enderta <endertanver@gmail.com>
2023-05-12 18:17:29 +0100

    mongo

--- server/mong.js
+++ server/mong.js
@@ -4,12 +4,12 @@ import BlogPost from "./schema.js";
 import connect from "./monconn.js";
 
 const app = express();
-
+app.use(express.json());
 connect();
-
 const insert = async (req, res) => {
     const { title, content, author } = req.body;
     const newPost = new BlogPost({
+        _id: new mongoose.Types.ObjectId(),
         title,
         content,
         author,
@@ -22,7 +22,7 @@ const insert = async (req, res) => {
     }
 };
 
-app.use(express.json());
+
 app.post("/posts", insert);
 
 const getPosts = async (req, res) => {
@@ -35,5 +35,54 @@ const getPosts = async (req, res) => {
 };
 
 app.get("/posts", getPosts);
+
+const getPost = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await BlogPost.findById(id);
+        res.status(200).json(post);
+    }
+    catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+app.get("/posts/:id", getPost);
+
+const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, content, author } = req.body;
+    try {
+        const post = await BlogPost.findById(id);
+        if (title) post.title = title;
+        if (content) post.content = content;
+        if (author) post.author = author;
+        await post.save();
+        res.status(200).json(post);
+    }
+    catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+app.put("/posts/:id", updatePost);
+
+const deletePost = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await BlogPost.findByIdAndRemove(id);
+        res.status(200).json({ message: "Post deleted successfully." });
+    }
+    catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+app.delete("/posts/:id", deletePost);
+
+
+
+
+
 
 app.listen(5000, () => console.log("Server Running"));
